13e9f22cb06e45dbee671b89ebbba1786e1bf452
VedantMaheshwari28 <vedantmaheshwari2006@gmail.com>
2025-03-25 12:28:11 +0530

    Add files via upload

---
+++ circularQueue.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#define N 5
+int cqueue[5];
+int front=-1,rear=-1,item;
+int isFull(){
+    return ((front==0&&rear==N-1)||(front==rear+1)) ;
+}
+int isEmpty(){
+    return front==-1&&rear==-1;
+}
+int enqueue(int num){
+    if(rear==-1&&front==-1){
+        front=rear=0;
+        cqueue[rear]=num;
+    }
+    else if(isFull()){
+            printf("OVERFLOW\n");
+        }
+    else
+    {
+        rear=(rear+1);
+        cqueue[rear]=num;
+    }
+}
+int dequeue(){
+    if(isEmpty()){
+        printf("UNDERFLOW\n");
+        return -1;
+    }
+    else if(rear==front){
+            item=cqueue[front];
+            front=rear=-1;
+            return item;
+        }
+    else {
+        item=cqueue[front];
+        front =(front+1);
+        return item;
+    }
+}
+void main()
+{
+    int m=5;
+    while( m>0){
+        enqueue(99);
+        m--;
+    }
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    printf("%d\n",dequeue());
+    
+    //printf("\n%d",isFull());
+}
